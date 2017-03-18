15f7093a35cedcbad6ae1ef4e7743fd9b2965b45
Raza Mehdi <srmk@outlook.com>
2017-03-18 11:02:56 +0500

    Fix spacing

--- config/config.php
+++ config/config.php
@@ -11,14 +11,14 @@ return [
         'password'    => env('PAYPAL_SANDBOX_API_PASSWORD', ''),
         'secret'      => env('PAYPAL_SANDBOX_API_SECRET', ''),
         'certificate' => env('PAYPAL_SANDBOX_API_CERTIFICATE', ''),
-        'app_id'      => 'APP-80W284485P519543T',    // Used for testing Adaptive Payments API in sandbox mode
+        'app_id'      => 'APP-80W284485P519543T', // Used for testing Adaptive Payments API in sandbox mode
     ],
     'live' => [
         'username'    => env('PAYPAL_LIVE_API_USERNAME', ''),
         'password'    => env('PAYPAL_LIVE_API_PASSWORD', ''),
         'secret'      => env('PAYPAL_LIVE_API_SECRET', ''),
         'certificate' => env('PAYPAL_LIVE_API_CERTIFICATE', ''),
-        'app_id'      => '',         // Used for Adaptive Payments API
+        'app_id'      => '', // Used for Adaptive Payments API
     ],
 
     'payment_action' => 'Sale', // Can only be 'Sale', 'Authorization' or 'Order'
