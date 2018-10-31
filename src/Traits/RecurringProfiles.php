36eb27a6df6db7139f6528b6b593417b46287ce0
Gabriel Ferreira Rosalino <eu@gabrielf.com>
2018-10-31 17:14:23 -0300

    Adding the RecurringProfiles::createCustomSubscription method (#241)

--- src/Traits/RecurringProfiles.php
+++ src/Traits/RecurringProfiles.php
@@ -7,6 +7,39 @@ use Carbon\Carbon;
 trait RecurringProfiles
 {
     /**
+     * Create recurring subscription on custom frequency basis.
+     * The billing frequency is in months, i.e: if you want a monthly subscription, then your billing frequency would be 1.
+     *
+     * @param string $token
+     * @param float $amount
+     * @param string $description
+     * @param int $billingFrequency
+     * @param int $trialDays
+     *
+     * @return array
+     */
+    public function createCustomSubscription($token, $amount, $description, $billingFrequency, $trialDays = null)
+    {
+        $data = [
+            'PROFILESTARTDATE' => Carbon::now()->toAtomString(),
+            'DESC'             => $description,
+            'BILLINGPERIOD'    => 'Month',
+            'BILLINGFREQUENCY' => $billingFrequency,
+            'AMT'              => $amount,
+            'CURRENCYCODE'     => $this->currency,
+        ];
+
+        if (!is_null($trialDays) && is_numeric($trialDays)) {
+            $data['TRIALBILLINGPERIOD'] = 'Day';
+            $data['TRIALTOTALBILLINGCYCLES'] = $trialDays;
+            $data['TRIALBILLINGFREQUENCY'] = 1;
+            $data['TRIALAMT'] = 0;
+        }
+
+        return $this->createRecurringPaymentsProfile($data, $token);
+    }
+
+    /**
      * Create recurring subscription on monthly basis.
      *
      * @param string $token
