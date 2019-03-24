824dfe9a7d56710825f12413562a3b2bd7847dfa
Stéphane Bour <sbour@welcome-media.fr>
2019-03-24 16:44:34 +0100

    chore: add french translation

---
+++ lang/fr/error.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'paypal_transaction_declined'     => 'Impossible de traiter le paiement car votre transaction a été refusée sur PayPal',
+    'paypal_transaction_not_verified' => 'Impossible de vérifier la transaction depuis PayPal',
+    'paypal_connection_error'         => 'Impossible de se connecter à PayPal. Veuillez réessayer',
+];
