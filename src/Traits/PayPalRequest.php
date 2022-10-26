cdf8dfa3d64c96a18e08c1440fac4b7be1cb06be
ilsyaa <ilham.shindou@gmail.com>
2022-10-26 18:03:45 +0700

    fix custom config

--- src/Traits/PayPalRequest.php
+++ src/Traits/PayPalRequest.php
@@ -143,7 +143,7 @@ trait PayPalRequest
      */
     private function setConfig(array $config): void
     {
-        $api_config = function_exists('config') && !empty(config('paypal')) ? config('paypal') : $config;
+        $api_config = $config ? $config : config('paypal');
 
         // Set Api Credentials
         $this->setApiCredentials($api_config);
