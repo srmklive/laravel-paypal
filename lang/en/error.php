9729bd89995978d1919daae3e28f2b6bbc840caf
Raza Mehdi <srmk@outlook.com>
2023-09-07 04:11:26 +0500

    Clearout

--- lang/en/error.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'paypal_transaction_declined'     => 'Unable To Process Payment As Your Transaction Was Declined On PayPal',
-    'paypal_transaction_not_verified' => 'Unable To Verify Transaction From PayPal',
-    'paypal_connection_error'         => 'Unable To Connect To PayPal. Please Try Again',
-];
