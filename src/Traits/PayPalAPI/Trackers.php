07e5cc5efa7d319d83c566975e2f9b55b1b46e51
Raza Mehdi <srmk@outlook.com>
2023-09-06 23:44:14 +0500

    Add endpoints for Add Tracking API.

--- src/Traits/PayPalAPI/Trackers.php
+++ src/Traits/PayPalAPI/Trackers.php
@@ -5,19 +5,64 @@ namespace Srmklive\PayPal\Traits\PayPalAPI;
 trait Trackers
 {
     /**
-     * Show tracking information.
+     * Adds tracking information, with or without tracking numbers, for multiple PayPal transactions.
      *
-     * @param string $tracking_id
+     * @param array $data
      *
      * @throws \Throwable
      *
      * @return array|\Psr\Http\Message\StreamInterface|string
      *
-     * @see https://developer.paypal.com/docs/api/tracking/v1/#trackers_get
+     * @see https://developer.paypal.com/docs/api/tracking/v1/#trackers-batch_post
      */
-    public function showTrackingDetails(string $tracking_id)
+    public function addBatchTracking(array $data)
     {
-        $this->apiEndPoint = "v1/shipping/trackers/{$tracking_id}";
+        $this->apiEndPoint = 'v1/shipping/trackers-batch';
+
+        $this->options['json'] = $data;
+
+        $this->verb = 'post';
+
+        return $this->doPayPalRequest();
+    }
+
+    /**
+     * Adds tracking information for a PayPal transaction.
+     *
+     * @param array $data
+     *
+     * @throws \Throwable
+     *
+     * @return array|\Psr\Http\Message\StreamInterface|string
+     *
+     * @see https://developer.paypal.com/docs/api/tracking/v1/#trackers_post
+     */
+    public function addTracking(array $data)
+    {
+        $this->apiEndPoint = 'v1/shipping/trackers';
+
+        $this->options['json'] = $data;
+
+        $this->verb = 'post';
+
+        return $this->doPayPalRequest();
+    }
+
+    /**
+     * List tracking information based on Transaction ID or tracking number.
+     *
+     * @param string $transaction_id
+     * @param string $tracking_number
+     *
+     * @throws \Throwable
+     *
+     * @return array|\Psr\Http\Message\StreamInterface|string
+     *
+     * @see https://developer.paypal.com/docs/api/tracking/v1/#trackers-batch_get
+     */
+    public function listTrackingDetails(string $transaction_id, string $tracking_number = null)
+    {
+        $this->apiEndPoint = "v1/shipping/trackers?transaction_id={$transaction_id}".!empty($tracking_number) ? "&tracking_number={$tracking_number}" : '';
 
         $this->verb = 'get';
 
@@ -48,23 +93,21 @@ trait Trackers
     }
 
     /**
-     * Add batch tracking.
+     * Show tracking information.
      *
-     * @param array $data
+     * @param string $tracking_id
      *
      * @throws \Throwable
      *
      * @return array|\Psr\Http\Message\StreamInterface|string
      *
-     * @see https://developer.paypal.com/docs/api/tracking/v1/#trackers-batch_post
+     * @see https://developer.paypal.com/docs/api/tracking/v1/#trackers_get
      */
-    public function addBatchTracking(array $data)
+    public function showTrackingDetails(string $tracking_id)
     {
-        $this->apiEndPoint = 'v1/shipping/trackers-batch';
+        $this->apiEndPoint = "v1/shipping/trackers/{$tracking_id}";
 
-        $this->options['json'] = $data;
-
-        $this->verb = 'post';
+        $this->verb = 'get';
 
         return $this->doPayPalRequest();
     }
--- tests/Feature/AdapterFeatureTest.php
+++ tests/Feature/AdapterFeatureTest.php
@@ -1908,6 +1908,28 @@ class AdapterFeatureTest extends TestCase
     }
 
     /** @test */
+    public function it_can_list_tracking_details()
+    {
+        $this->client->setAccessToken([
+            'access_token'  => self::$access_token,
+            'token_type'    => 'Bearer',
+        ]);
+
+        $this->client->setClient(
+            $this->mock_http_client(
+                $this->mockGetTrackingDetailsResponse()
+            )
+        );
+
+        $response = $this->client->listTrackingDetails('8MC585209K746392H-443844607820');
+
+        $this->assertNotEmpty($response);
+        $this->assertEquals($response, $this->mockGetTrackingDetailsResponse());
+        $this->assertArrayHasKey('transaction_id', $response);
+        $this->assertArrayHasKey('tracking_number', $response);
+    }
+
+    /** @test */
     public function it_can_get_tracking_details_for_tracking_id()
     {
         $this->client->setAccessToken([
@@ -1965,6 +1987,28 @@ class AdapterFeatureTest extends TestCase
         $expectedParams = $this->mockCreateTrackinginBatchesParams();
 
         $response = $this->client->addBatchTracking($expectedParams);
+
+        $this->assertNotEmpty($response);
+        $this->assertArrayHasKey('tracker_identifiers', $response);
+    }
+
+    /** @test */
+    public function it_can_create_single_tracking_for_single_transaction()
+    {
+        $this->client->setAccessToken([
+            'access_token'  => self::$access_token,
+            'token_type'    => 'Bearer',
+        ]);
+
+        $this->client->setClient(
+            $this->mock_http_client(
+                $this->mockCreateTrackinginBatchesResponse()
+            )
+        );
+
+        $expectedParams = $this->mockCreateTrackinginBatchesParams();
+
+        $response = $this->client->addTracking($expectedParams);
 
         $this->assertNotEmpty($response);
         $this->assertArrayHasKey('tracker_identifiers', $response);
