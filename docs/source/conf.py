299ca429590cee57cfed6612d223c8b461195fe5
Raza Mehdi <srmk@outlook.com>
2023-09-07 05:36:52 +0500

    Try to fix build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx_new_tab_link",
+    # "sphinx_new_tab_link",
     "sphinx.ext.githubpages",
 ]
 
