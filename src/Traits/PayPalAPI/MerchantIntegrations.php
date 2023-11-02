89f4f4cf8eca61e18a8a6dc592d5126d58d5835a
Josef Behr <code@josef-behr.de>
2023-11-02 14:32:33 +0100

    add method to show seller status

---
+++ src/Traits/PayPalAPI/MerchantIntegrations.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Srmklive\PayPal\Traits\PayPalAPI;
+
+trait MerchantIntegrations
+{
+    /**
+     * Shows seller status
+     *
+     * @param string $partner_id
+     *
+     * @param string $merchant_id
+     *
+     * @return mixed
+     */
+    public function showSellerStatus(string $partner_id, string $merchant_id)
+    {
+        $this->apiEndPoint = "v1/customer/partners/{$partner_id}/merchant-integrations/{$merchant_id}";
+
+        $this->verb = 'get';
+
+        return $this->doPayPalRequest();
+    }
+}
