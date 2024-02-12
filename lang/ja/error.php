61a6ac176cebe1addb28c3ce4b96444589467c0c
matheo-2001 <collinamatheo2001@gmail.com>
2024-02-12 18:32:07 +0100

    Add lang ja

---
+++ lang/ja/error.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'paypal_transaction_declined'     => 'PayPalでお取引が拒否されたため、支払いを処理できません',
+    'paypal_transaction_not_verified' => 'PayPalからの取引を確認できません',
+    'paypal_connection_error'         => 'PayPalに接続できません。もう一度お試しください',
+];
